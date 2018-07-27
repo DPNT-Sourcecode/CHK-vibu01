9f0cbd2722b70e22bf1fb6a87a1816b9e9b97cc1
sbx_user1061 <sbx_user1061@ip-10-12-0-55.eu-west-2.compute.internal>
2018-07-27 20:51:23 +0000

    Fri Jul 27 20:51:23 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -12,12 +12,12 @@ def checkout(skus):
 
    count_of_skus = dict(Counter(list_of_skus))
    price = {"A":50,"B":30,"C":20,"D":15,"E":40}
-
+   cost = 0
 
    if "A" in list_of_skus:
     number_of_a = count_of_skus["A"]
     a_offer_count = (number_of_a / 3)
-    cost = (a_offer_count) * 130
+    cost += (a_offer_count) * 130
     if a_offer_count >= 1:
         remainder = count_of_skus["A"] % 3
         print "remainder" + str(remainder)
@@ -27,7 +27,7 @@ def checkout(skus):
    if "B" in list_of_skus:
     number_of_b = count_of_skus["B"]
     b_offer_count = (number_of_a / 2)
-    cost = cost =+ (b_offer_count) * 45
+    cost +=  (b_offer_count) * 45
     if b_offer_count >= 1:
         remainder = count_of_skus["B"] % 2
         print "remainder" + str(remainder)

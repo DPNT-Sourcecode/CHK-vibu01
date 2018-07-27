623b2fdf3b3c7892f143565ba308da076c50dacf
sbx_user1061 <sbx_user1061@ip-10-12-0-55.eu-west-2.compute.internal>
2018-07-27 20:45:23 +0000

    Fri Jul 27 20:45:23 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,41 +3,36 @@ from collections import Counter
 # skus = unicode string
 
 def checkout(skus):
-   allowed_sku_list = ['A','B','C','D']
+   allowed_sku_list = ['A','B','C','D','E']
    list_of_skus = list(skus)
 
    for sku in list_of_skus:
        if sku not in allowed_sku_list:
            return -1
 
-
    count_of_skus = dict(Counter(list_of_skus))
+   price = {"A":50,"B":30,"C":20,"D":15,"E":40}
 
 
-
-
-   price = {"A":50,"B":30,"C":20,"D":15}
-   try:
-       number_of_a= count_of_skus["A"]
-   except KeyError:
-       number_of_a = 0
+   if "A" in list_of_skus:
+    number_of_a = count_of_skus["A"]
+    number_of_a = 0
+    a_offer_count = (number_of_a / 3)
+    cost = (a_offer_count) * 130
+    if a_offer_count >= 1:
+        remainder = count_of_skus["A"] % 3
+        print "remainder" + str(remainder)
+        count_of_skus["A"] = remainder
 
    try:
        number_of_b= count_of_skus["B"]
    except KeyError:
        number_of_b = 0
 
-   a_offer_count =  (number_of_a / 3)
-   cost =  (a_offer_count) * 130
 
 
-   if a_offer_count >= 1:
-       try:
-           remainder = count_of_skus["A"] % 3
-           print "remainder" + str(remainder)
-           count_of_skus["A"] = remainder
-       except KeyError:
-           pass
+
+
 
    b_offer_count =  (number_of_b / 2)
    cost =  cost + (b_offer_count) * 45
@@ -58,6 +53,8 @@ def checkout(skus):
 
 
    return cost
+
+checkout("BBBB")
 
 
 

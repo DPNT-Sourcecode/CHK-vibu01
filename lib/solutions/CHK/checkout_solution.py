f6eb322670be4376de63e2345d7e740bc2a18d2a
sbx_user1061 <sbx_user1061@ip-10-12-0-55.eu-west-2.compute.internal>
2018-07-27 19:44:57 +0000

    Fri Jul 27 19:44:57 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,7 +5,10 @@ from collections import Counter
 def checkout(skus):
    list_of_skus = list(skus)
    count_of_skus = dict(Counter(list_of_skus))
-   print count_of_skus
 
-   print [sku for sku in skus if not sku.isalpha()]
-checkout("ABCDABCD")
+   non_alphabetical_skus =  [sku for sku in skus if not sku.isalpha()]
+   if non_alphabetical_skus:
+       return -1
+
+   print count_of_skus("A")
+checkout("ABCDABCD2")

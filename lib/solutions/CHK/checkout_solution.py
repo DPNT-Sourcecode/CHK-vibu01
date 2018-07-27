c29c2c0e737bd465f112f05d141810420764def1
sbx_user1060 <sbx_user1060@ip-10-11-30-10.eu-west-2.compute.internal>
2018-07-27 22:08:24 +0000

    Fri Jul 27 22:08:24 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -61,7 +61,13 @@ def checkout(skus):
     if count_of_skus["E"] >= 2:
         multi_offer_one_free("E", 2, "B")
     else :
-        cost = multi_offer("B", 2, 45, cost)
+         multi_offer("B", 2, 45, cost)
+   elif "E" in list_of_skus:
+       multi_offer_one_free("E", 2, "B")
+   elif "B" in list_of_skus:
+       multi_offer("B", 2, 45, cost
+
+
 
 
 
@@ -72,3 +78,9 @@ def checkout(skus):
 
 
    return cost
+
+print checkout("BB")
+
+# - {"method": "checkout", "params": ["BB"], "id": "CHK_R2_036"}, expected: 45, got: 60
+# - {"method": "checkout", "params": ["BBB"], "id": "CHK_R2_037"}, expected: 75, got: 90
+# - {"method": "checkout", "params": ["BBBB"], "id": "CHK_R2_038"}, expected: 90, got: 120

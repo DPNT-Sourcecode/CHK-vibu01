5762ea8c851055634fa9fc8f77857d8109c8e751
sbx_user1061 <sbx_user1061@ip-10-12-0-55.eu-west-2.compute.internal>
2018-07-27 19:38:57 +0000

    Fri Jul 27 19:38:57 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,4 +1,9 @@
+from collections import Counter
 # noinspection PyUnusedLocal
 # skus = unicode string
+
 def checkout(skus):
-   list_of_skus = skus.split("")
+   list_of_skus = list(skus)
+   count_of_skus = dict(Counter(list_of_skus))
+   print count_of_skus
+checkout("ABCDABCD")

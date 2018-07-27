af9a4849ca6e025b884d69ec0637739d8a583c85
sbx_user1061 <sbx_user1061@ip-10-12-0-55.eu-west-2.compute.internal>
2018-07-27 21:03:23 +0000

    Fri Jul 27 21:03:23 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,11 +2,6 @@ from collections import Counter
 # noinspection PyUnusedLocal
 # skus = unicode string
 
-
-
-
-
-
 def checkout(skus):
    allowed_sku_list = ['A','B','C','D','E']
    list_of_skus = list(skus)
@@ -20,23 +15,17 @@ def checkout(skus):
    cost = 0
 
    def multi_offer(sku,quanity, price,cost):
-       print cost
-       print sku
-       number_of_a = count_of_skus[sku]
-       print number_of_a
-
-       a_offer_count = (number_of_a / quanity)
-       print a_offer_count
-       cost += (a_offer_count) * price
-       print cost
-       if a_offer_count >= 1:
+       number_of_sku = count_of_skus[sku]
+       sku_offer_count = (number_of_sku / quanity)
+       cost += (sku_offer_count) * price
+       if sku_offer_count >= 1:
            remainder = count_of_skus[sku] % quanity
-           print "remainder" + str(remainder)
            count_of_skus[sku] = remainder
        return cost
 
-   cost += multi_offer("A",3,130,cost)
-   cost += multi_offer("B", 2, 45, cost)
+   cost = multi_offer("A",3,130,cost)
+   cost = multi_offer("B", 2, 45, cost)
+
 
 
 
@@ -44,13 +33,12 @@ def checkout(skus):
 
    print count_of_skus
    for sku,value in count_of_skus.iteritems():
-        print value
         cost += value *price[sku]
 
 
    return cost
 
-print checkout("AAABB")
+print checkout("AAAEE")
 
 
 

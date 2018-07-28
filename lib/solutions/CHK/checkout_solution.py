53baf6a9b5f24b90fe6925cf797854615316574d
sbx_user1061 <sbx_user1061@ip-10-12-15-7.eu-west-2.compute.internal>
2018-07-28 21:41:00 +0000

    Sat Jul 28 21:41:00 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,7 +3,8 @@ from collections import Counter
 # skus = unicode string
 
 def checkout(skus):
-   allowed_sku_list = ['A','B','C','D','E',"F"]
+   allowed_sku_list = ['A','B','C','D','E',"F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W",
+                       "X","Y","Z"]
    list_of_skus = list(skus)
 
    for sku in list_of_skus:
@@ -12,7 +13,8 @@ def checkout(skus):
 
    count_of_skus = dict(Counter(list_of_skus))
    print count_of_skus
-   price = {"A":50,"B":30,"C":20,"D":15,"E":40,"F":10}
+   price = {"A":50,"B":30,"C":20,"D":15,"E":40,"F":10,"G":10,"H":10,"I":35,"J":60,"K":80,"L":90,"M":15,"N":40,"O":10,
+            "P":50,"Q":30,"R":50,"S":30,"T":20,"U":40,"V":50,"W":20,"X":90,"Y":10,"Z":50}
    cost = 0
 
    def multi_offer(sku,quanity, price,cost):
@@ -70,6 +72,62 @@ def checkout(skus):
 
    print cost
 
+   if "H" in list_of_skus:
+        while count_of_skus["H"] >= 5:
+            if count_of_skus["H"] >= 10:
+                cost = multi_offer("H", 10, 80, cost)
+
+            if count_of_skus["H"] >= 5:
+                cost = multi_offer("H",5,45,cost)
+
+   print cost
+
+   if "P" in list_of_skus:
+        while count_of_skus["P"] >= 5:
+
+            if count_of_skus["P"] >= 5:
+                cost = multi_offer("P",5,200,cost)
+
+   print cost
+
+   if "Q" in list_of_skus:
+        while count_of_skus["Q"] >= 3:
+
+            if count_of_skus["Q"] >= 3:
+                cost = multi_offer("Q",3,80,cost)
+
+   print cost
+
+   if "V" in list_of_skus:
+        while count_of_skus["V"] >= 2:
+            if count_of_skus["V"] >= 3:
+                cost = multi_offer("V", 3, 130, cost)
+
+            if count_of_skus["V"] >= 2:
+                cost = multi_offer("V",2,90,cost)
+
+   print cost
+
+   if list_of_skus.count("K") >= 2:
+       while count_of_skus["K"] >= 2:
+           cost = multi_offer("K", 2, 150, cost)
+
+   if list_of_skus.count("N") >= 3 and list_of_skus.count("M"):
+       print "N was found " + str(list_of_skus.count("N")) + " times."
+       apply_count = list_of_skus.count("N") / 3
+       print "The N offer can be applied " + str(apply_count)
+
+       multi_offer_one_free("N", 3, "M")
+
+   if list_of_skus.count("R") >= 3 and list_of_skus.count("Q"):
+       print "R was found " + str(list_of_skus.count("R")) + " times."
+       apply_count = list_of_skus.count("R") / 3
+       print "The R offer can be applied " + str(apply_count)
+
+       multi_offer_one_free("R", 3, "Q")
+
+
+
 
    if list_of_skus.count("E") >= 2 and list_of_skus.count("B"):
        print "E was found " + str(list_of_skus.count("E")) + " times."
@@ -94,6 +152,10 @@ def checkout(skus):
    if list_of_skus.count("F") >= 3:
        print "F was found " + str(list_of_skus.count("F")) + " times."
        multi_offer_one_free_same("F", 2)
+
+   if list_of_skus.count("U") >= 4:
+       print "U was found " + str(list_of_skus.count("U")) + " times."
+       multi_offer_one_free_same("U", 3)
 
 
 
